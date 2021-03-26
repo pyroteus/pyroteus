ac84528c25b6be0e0484facf23372f861bfb27e5
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-03-26 10:06:13 +0000

    Make a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyroteus',
+    description='Pyroteus Goal-Oriented Mesh Adaptation Toolkit',
+    author='Joseph Wallwork',
+    author_email='j.wallwork16@imperial.ac.uk',
+    url='https://github.com/jwallwork23/pyroteus',
+    version='0.1',
+    packages=['pyroteus'],
+)
