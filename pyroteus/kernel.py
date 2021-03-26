310098c7b2b12c4b70aa7018e66b4e553e2d99db
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-03-26 08:36:30 +0000

    kernel: metric post-processing

--- pyroteus/kernel.py
+++ pyroteus/kernel.py
@@ -13,3 +13,44 @@ def eigen_kernel(kernel, *args, **kwargs):
     Helper function to easily pass Eigen kernels to Firedrake via PyOP2.
     """
     return op2.Kernel(kernel(*args, **kwargs), kernel.__name__, cpp=True, include_dirs=include_dir)
+
+
+def postproc_metric(d, h_min, h_max, a_max):
+    """
+    Post-process a metric field in order to enforce max/min element sizes and anisotropy.
+
+    :arg d: spatial dimension.
+    :arg h_min: minimum element size.
+    :arg h_max: maximum element size.
+    :arg a_max: maximum element anisotropy.
+    """
+    kernel_str = """
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+void postproc_metric(double A_[%d])
+{
+
+  // Map input/output metric onto an Eigen object
+  Map<Matrix<double, %d, %d, RowMajor> > A((double *)A_);
+
+  // Solve eigenvalue problem
+  SelfAdjointEigenSolver<Matrix<double, %d, %d, RowMajor>> eigensolver(A);
+  Matrix<double, %d, %d, RowMajor> Q = eigensolver.eigenvectors();
+  Vector%dd D = eigensolver.eigenvalues();
+
+  // Scale eigenvalues appropriately
+  int i;
+  double max_eig = 0.0;
+  for (i=0; i<%d; i++) {
+    D(i) = fmin(pow(%f, -2), fmax(pow(%f, -2), abs(D(i))));
+    max_eig = fmax(max_eig, D(i));
+  }
+  for (i=0; i<%d; i++) D(i) = fmax(D(i), pow(%f, -2) * max_eig);
+
+  // Build metric from eigendecomposition
+  A = Q * D.asDiagonal() * Q.transpose();
+}
+"""
+    return kernel_str % (d*d, d, d, d, d, d, d, d, d, h_min, h_max, d, a_max)
