786a06d3628ed4d454e1260dfefa7ca5a1b2a89e
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-03-26 12:57:58 +0000

    test: density/quotients decomposition

--- test/test_decompositions.py
+++ test/test_decompositions.py
@@ -1,3 +1,6 @@
+"""
+Test matrix decomposition par_loops.
+"""
 from pyroteus import *
 import pyroteus.kernel as kernels
 import pytest
@@ -26,8 +29,7 @@ def test_eigendecomposition(dim):
     metric = hessian_metric(recover_hessian(f, mesh=mesh))
 
     # Extract the eigendecomposition
-    evectors = Function(P1_ten)
-    evalues = Function(P1_vec)
+    evectors, evalues = Function(P1_ten), Function(P1_vec)
     kernel = kernels.eigen_kernel(kernels.get_eigendecomposition, dim)
     op2.par_loop(kernel, P1_ten.node_set,
                  evectors.dat(op2.RW), evalues.dat(op2.RW), metric.dat(op2.READ))
@@ -42,4 +44,33 @@ def test_eigendecomposition(dim):
     kernel = kernels.eigen_kernel(kernels.set_eigendecomposition, dim)
     op2.par_loop(kernel, P1_ten.node_set,
                  reassembled.dat(op2.RW), evectors.dat(op2.READ), evalues.dat(op2.READ))
-    assert np.allclose(metric.dat.data, reassembled.dat.data)
+    metric -= reassembled
+    assert np.isclose(norm(metric), 0.0)
+
+
+def test_density_quotients_decomposition(dim):
+    mesh = uniform_mesh(dim, 20, 2)
+    P1_vec = VectorFunctionSpace(mesh, "CG", 1)
+    P1_ten = TensorFunctionSpace(mesh, "CG", 1)
+
+    # Recover Hessian metric for some arbitrary sensor
+    f = prod([sin(pi*xi) for xi in SpatialCoordinate(mesh)])
+    metric = hessian_metric(recover_hessian(f, mesh=mesh))
+
+    # Extract the eigendecomposition
+    evectors, evalues = Function(P1_ten), Function(P1_vec)
+    kernel = kernels.eigen_kernel(kernels.get_eigendecomposition, dim)
+    op2.par_loop(kernel, P1_ten.node_set,
+                 evectors.dat(op2.RW), evalues.dat(op2.RW), metric.dat(op2.READ))
+
+    # Extract the density and anisotropy quotients
+    d, Q = density_and_quotients(metric)
+    evalues.interpolate(as_vector([pow(d/Q[i], 2/dim) for i in range(dim)]))
+
+    # Reassemble the matrix and check the two match
+    reassembled = Function(P1_ten)
+    kernel = kernels.eigen_kernel(kernels.set_eigendecomposition, dim)
+    op2.par_loop(kernel, P1_ten.node_set,
+                 reassembled.dat(op2.RW), evectors.dat(op2.READ), evalues.dat(op2.READ))
+    metric -= reassembled
+    assert np.isclose(norm(metric), 0.0)
