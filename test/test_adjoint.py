40ad0a289bf8946d4e126b3a3b95012b01a45745
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-04-07 15:21:39 +0100

    test: test_adjoint_burgers_same_mesh

---
+++ test/test_adjoint.py
@@ -0,0 +1,130 @@
+from pyroteus import *
+from firedrake_adjoint import *
+
+
+def solve_burgers(ic, t_start, t_end, dt, **kwargs):
+    """
+    Solve Burgers' equation on an interval
+    (t_start, t_end), given viscosity `nu` and some
+    initial condition `ic`.
+    """
+    fs = ic.function_space()
+    dtc = Constant(dt)
+
+    # Check viscosity parameter gets passed through
+    nu = kwargs.get('viscosity')
+    if nu is None:
+        raise ValueError
+
+    # Set initial condition
+    u_ = Function(fs)
+    u_.assign(ic)
+
+    # Setup variational problem
+    v = TestFunction(fs)
+    u = Function(fs)
+    F = inner((u - u_)/dtc, v)*dx \
+        + inner(dot(u, nabla_grad(u)), v)*dx \
+        + nu*inner(grad(u), grad(v))*dx
+
+    # Time integrate from t_start to t_end
+    t = t_start
+    while t < t_end - 1.0e-05:
+        solve(F == 0, u)
+        u_.assign(u)
+        t += dt
+    return u_
+
+
+def initial_condition_burgers(fs):
+    """
+    Initial condition for Burgers' equation
+    which is sinusoidal in the x-direction.
+
+    :arg fs: :class:`FunctionSpace` which
+        the initial condition will live in
+    """
+    x, y = SpatialCoordinate(fs.mesh())
+    return interpolate(as_vector([sin(pi*x), 0]), fs)
+
+
+def quantity_of_interest_burgers(sol):
+    """
+    Quantity of interest for Burgers' equation
+    which computes the square L2 norm over the
+    right hand boundary segment.
+
+    :arg sol: the solution :class:`Function`
+    """
+    return inner(sol, sol)*ds(2)
+
+
+# ---------------------------
+# standard tests for pytest
+# ---------------------------
+
+def test_adjoint_burgers_same_mesh(plot=False):
+    """
+    Check that `solve_adjoint` gives the same
+    result when applied on one or two subintervals.
+
+    :kwarg plot: toggle plotting of the adjoint
+        solution field
+    """
+    n = 32
+    mesh = UnitSquareMesh(n, n, diagonal='left')
+    end_time = 0.5
+    dt = 1/n
+
+    # Loop over having one or two subintervals
+    final_adj_sols = []
+    for meshes in ([mesh], [mesh, mesh]):
+        dt_per_export = 2
+        N = len(meshes)
+        subintervals = get_subintervals(end_time, N)
+
+        # Solve forward and adjoint on each subinterval
+        adj_sols = solve_adjoint(
+            solve_burgers,
+            initial_condition_burgers,
+            quantity_of_interest_burgers,
+            [VectorFunctionSpace(mesh, "CG", 2) for mesh in meshes],
+            end_time,
+            dt,
+            timesteps_per_export=dt_per_export,
+            solver_kwargs=dict(viscosity=Constant(0.0001)),
+        )
+        final_adj_sols.append(adj_sols[-1][-1])
+
+        # Plot adjoint solutions, if requested
+        if plot:
+            import matplotlib.pyplot as plt
+            from pyroteus.adjoint import _get_exports_per_subinterval
+
+            levels = np.linspace(0, 0.8, 9)
+            _, dt_per_export, exports_per_mesh = \
+                _get_exports_per_subinterval(subintervals, dt, dt_per_export)
+            fig, axes = plt.subplots(exports_per_mesh[0], N, sharex='col', figsize=(6*N, 24//N))
+            for i, adj_sols_step in enumerate(adj_sols):
+                ax = axes[0] if N == 1 else axes[0, i]
+                ax.set_title("Mesh {:d}".format(i+1))
+                for j, adj_sol in enumerate(adj_sols_step):
+                    ax = axes[j] if N == 1 else axes[j, i]
+                    tricontourf(adj_sol, axes=ax, levels=levels)
+                    ax.annotate(
+                        f"t={(N-i)*end_time/N - j*dt_per_export[0]*dt:.2f}",
+                        (0.05, 0.05),
+                        color='white',
+                    )
+            plt.savefig(f"plots/burgers_test_{N}.jpg")
+
+    # Check adjoint solutions match
+    np.isclose(errornorm(*final_adj_sols)/norm(final_adj_sols[0]), 0.0)
+
+
+# ---------------------------
+# plotting and debugging
+# ---------------------------
+
+if __name__ == "__main__":
+    test_adjoint_burgers(plot=True)
