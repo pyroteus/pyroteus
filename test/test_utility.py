f593868ab456243a6cebdf80a7cf92df355d2dfd
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-04-14 11:40:53 +0100

    test: check hessian_metric does not affect SPD matrix

--- test/test_utility.py
+++ test/test_utility.py
@@ -17,9 +17,25 @@ def dim(request):
 
 
 def test_is_spd(dim):
+    """
+    Check `is_spd` correctly identifies an SPD
+    and a non-SPD matrix.
+    """
     mesh = uniform_mesh(dim, 4)
     P1_ten = TensorFunctionSpace(mesh, "CG", 1)
     M = interpolate(Identity(dim), P1_ten)
     assert is_spd(M)
     M.sub(0).assign(-1)
     assert not is_spd(M)
+
+
+def test_hessian_metric(dim):
+    """
+    Check that `hessian_metric` does not have
+    an effect on a matrix that is already SPD.
+    """
+    mesh = uniform_mesh(dim, 4)
+    P1_ten = TensorFunctionSpace(mesh, "CG", 1)
+    M = interpolate(Identity(dim), P1_ten)
+    M -= hessian_metric(M)
+    assert np.isclose(norm(M), 0.0)
