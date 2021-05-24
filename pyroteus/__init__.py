4faee04fc41bd26b84a318c41539f9c88284ac02
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-05-24 17:18:17 +0100

    docs: use same base setup as Thetis

--- pyroteus/__init__.py
+++ pyroteus/__init__.py
@@ -3,3 +3,5 @@ from pyroteus.interpolation import *  # noqa
 from pyroteus.metric import *         # noqa
 from pyroteus.recovery import *       # noqa
 from pyroteus.utility import *        # noqa
+
+__version__ = '0.1'
