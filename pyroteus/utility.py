1a764dae52acb6120c019917980d17b2716df374
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-05-28 11:23:36 +0100

    utility: remove NotImplementedError

--- pyroteus/utility.py
+++ pyroteus/utility.py
@@ -46,29 +46,27 @@ def Mesh(arg, **kwargs):
     else:
         mesh.boundary_area = OrderedDict({i: assemble(one*ds(int(i))) for i in boundary_markers})
 
-    if not extras:
-        return mesh
-    if dim != 2 or mesh.coordinates.ufl_element().cell() != triangle:
-        raise NotImplementedError  # TODO
-    P0_ten = TensorFunctionSpace(mesh, "DG", 0)
-    J = interpolate(Jacobian(mesh), P0_ten)
-    edge1 = as_vector([J[0, 0], J[1, 0]])
-    edge2 = as_vector([J[0, 1], J[1, 1]])
-    edge3 = edge1 - edge2
-    a = sqrt(dot(edge1, edge1))
-    b = sqrt(dot(edge2, edge2))
-    c = sqrt(dot(edge3, edge3))
+    # Compute aspect ratio and scaled Jacobian
+    if extras and dim == 2 and mesh.coordinates.ufl_element().cell() == triangle:
+        P0_ten = TensorFunctionSpace(mesh, "DG", 0)
+        J = interpolate(Jacobian(mesh), P0_ten)
+        edge1 = as_vector([J[0, 0], J[1, 0]])
+        edge2 = as_vector([J[0, 1], J[1, 1]])
+        edge3 = edge1 - edge2
+        a = sqrt(dot(edge1, edge1))
+        b = sqrt(dot(edge2, edge2))
+        c = sqrt(dot(edge3, edge3))
 
-    # Aspect ratio
-    if ar:
-        mesh.aspect_ratio = interpolate(a*b*c/((a+b-c)*(b+c-a)*(c+a-b)), P0)
+        # Aspect ratio
+        if ar:
+            mesh.aspect_ratio = interpolate(a*b*c/((a+b-c)*(b+c-a)*(c+a-b)), P0)
 
-    # Scaled Jacobian
-    if sj:
-        detJ = JacobianDeterminant(mesh)
-        jacobian_sign = sign(detJ)
-        max_product = Max(Max(Max(a*b, a*c), Max(b*c, b*a)), Max(c*a, c*b))
-        mesh.scaled_jacobian = interpolate(detJ/max_product*jacobian_sign, P0)
+        # Scaled Jacobian
+        if sj:
+            detJ = JacobianDeterminant(mesh)
+            jacobian_sign = sign(detJ)
+            max_product = Max(Max(Max(a*b, a*c), Max(b*c, b*a)), Max(c*a, c*b))
+            mesh.scaled_jacobian = interpolate(detJ/max_product*jacobian_sign, P0)
 
     return mesh
 
