908083005df8f7f5b2415e89b3ce13e23fa325cc
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-06-04 12:19:08 +0100

    adjoint: adj_values

--- pyroteus/adjoint.py
+++ pyroteus/adjoint.py
@@ -56,7 +56,7 @@ def count_qoi_args(qoi):
         raise ValueError(f"QoI should have 1 or 2 args, not {num_args}")
 
 
-def wrap_qoi(qoi):
+def wrap_qoi(qoi, adj_value=1.0):
     """
     Wrapper for contributions to a quantity of interest which sets
     the corresponding adj_value to unity.
@@ -68,7 +68,7 @@ def wrap_qoi(qoi):
     @wraps(qoi)
     def wrapper(*args, **kwargs):
         j = firedrake.assemble(qoi(*args, **kwargs))
-        j.block_variable.adj_value = 1.0
+        j.block_variable.adj_value = adj_value
         return j
 
     return wrapper, count_qoi_args(qoi)
@@ -111,6 +111,8 @@ def solve_adjoint(solver, initial_condition, qoi, function_spaces, time_partitio
         containing the subintervals
     :kwarg solver_kwargs: a dictionary providing parameters
         to the solver
+    :kwarg get_adj_values: additionally output adjoint
+        actions at exported timesteps
 
     :return J: quantity of interest value
     :return solution: an :class:`AttrDict` containing
@@ -127,6 +129,9 @@ def solve_adjoint(solver, initial_condition, qoi, function_spaces, time_partitio
         print("WARNING: Zero QoI. Is it implemented as intended?")
 
     # Create arrays to hold exported foward and adjoint solutions
+    labels = ('forward', 'forward_old', 'adjoint')
+    if not kwargs.get('steady', False):
+        labels += ('adjoint_next',)
     solutions = AttrDict({
         field: AttrDict({
             label: [
@@ -134,9 +139,22 @@ def solve_adjoint(solver, initial_condition, qoi, function_spaces, time_partitio
                     firedrake.Function(fs)
                     for j in range(time_partition.exports_per_subinterval[i]-1)
                 ] for i, fs in enumerate(function_spaces[field])
-            ] for label in ('forward', 'forward_old', 'adjoint', 'adjoint_next')
+            ] for label in labels
         }) for field in fields
     })
+
+    # Create arrays to hold adjoint actions
+    if kwargs.get('get_adj_values', False):
+        adj_values = AttrDict({
+            field: [
+                [
+                    firedrake.Function(fs)
+                    for j in range(time_partition.exports_per_subinterval[i]-1)
+                ] for i, fs in enumerate(function_spaces[field])
+            ] for field in fields
+        })
+    else:
+        adj_values = None
 
     # Wrap solver and QoI
     wrapped_solver = wrap_solver(solver)
@@ -194,7 +212,7 @@ def solve_adjoint(solver, initial_condition, qoi, function_spaces, time_partitio
             # Get lagged forward solution dependency index
             fwd_old_idx = [
                 dep_index
-                for dep_index, dep in enumerate(solve_blocks[0].get_dependencies())
+                for dep_index, dep in enumerate(solve_blocks[0]._dependencies)
                 if hasattr(dep.output, 'function_space')
                 and dep.output.function_space() == solve_blocks[0].function_space == fs[i]
             ]
@@ -211,6 +229,8 @@ def solve_adjoint(solver, initial_condition, qoi, function_spaces, time_partitio
             for j, block in enumerate(solve_blocks[::stride]):
                 sols.forward[i][j].assign(block.get_outputs()[0].saved_output)
                 sols.adjoint[i][j].assign(block.adj_sol)
+                if adj_values is not None:
+                    adj_values[field][i][j].assign(block._dependencies[fwd_old_idx].adj_value.function)
                 sols.forward_old[i][j].assign(block._dependencies[fwd_old_idx].saved_output)
                 if not steady:
                     if j*stride+1 < num_solve_blocks:
@@ -225,6 +245,8 @@ def solve_adjoint(solver, initial_condition, qoi, function_spaces, time_partitio
                                          + f"> {num_solve_blocks}")
             if np.isclose(norm(solutions[field].adjoint[i][0]), 0.0):
                 print(f"WARNING: Adjoint solution for field {field} on subinterval {i} is zero.")
+            if adj_values is not None and np.isclose(norm(adj_values[field][i][0]), 0.0):
+                print(f"WARNING: Adjoint action for field {field} on subinterval {i} is zero.")
 
         # Get adjoint action
         seeds = {
@@ -239,7 +261,11 @@ def solve_adjoint(solver, initial_condition, qoi, function_spaces, time_partitio
                     print("  You seem to have a steady-state problem. Presumably it is linear?")
         tape.clear_tape()
 
-    return J, solutions
+    # Process return values
+    ret = (J, solutions)
+    if adj_values is not None:
+        ret += (adj_values,)
+    return ret
 
 
 @pyadjoint.no_annotations
