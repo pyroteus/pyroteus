c2cb6728040988499acf34725b57a067b0f138ff
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-06-09 17:22:33 +0100

    go_mesh_seq: add to pyroteus_adjoint.__init__

--- pyroteus_adjoint/__init__.py
+++ pyroteus_adjoint/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
 from pyroteus import *                # noqa
 from pyroteus.adjoint import *        # noqa
+from pyroteus.go_mesh_seq import *    # noqa
 from pyadjoint import no_annotations  # noqa
