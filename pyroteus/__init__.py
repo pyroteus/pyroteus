4aed900248a91d63bc359f89ede5e1522fc5747b
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-06-14 10:36:33 +0100

    plot: plot_snapshots

--- pyroteus/__init__.py
+++ pyroteus/__init__.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from pyroteus.utility import *         # noqa
 from pyroteus.time_partition import *  # noqa
 from pyroteus.recovery import *        # noqa
+from pyroteus.plot import *            # noqa
 from pyroteus.metric import *          # noqa
 from pyroteus.mesh_seq import *        # noqa
 from pyroteus.interpolation import *   # noqa
