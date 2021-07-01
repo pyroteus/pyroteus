e3c74e573305966429cd6636b9c21829ac139d37
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-07-01 11:18:30 +0100

    error_estimation: get_dwr_indicator

--- pyroteus/error_estimation.py
+++ pyroteus/error_estimation.py
@@ -1,11 +1,16 @@
 from firedrake import *
 
 
+__all__ = ["get_dwr_indicator"]
+
+
 def form2indicator(F):
     """
     Multiply throughout in a form and
     assemble as a cellwise error
     indicator.
+
+    :arg F: the form
     """
     mesh = F.ufl_domain()
     P0 = FunctionSpace(mesh, "DG", 0)
@@ -37,4 +42,18 @@ def form2indicator(F):
     return indicator
 
 
-# TODO: Version which swaps TestFunctions out for adjoint error
+def get_dwr_indicator(F, adjoint_error):
+    """
+    Generate a dual weighted residual (DWR)
+    error indicator, given a form and an
+    approximation of the error in the adjoint
+    solution.
+
+    :arg F: the form
+    :arg adjoint_error: the approximation to
+        the adjoint error
+    """
+    fs = adjoint_error.function_space()
+    if F.ufl_domain() != fs.mesh():
+        raise ValueError("Meshes underlying the form and adjoint error do not match.")
+    return form2indicator(replace(F, {TestFunction(fs): adjoint_error}))
