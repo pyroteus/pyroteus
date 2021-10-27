7bd53568be23743def1e312daaf1908fb946a932
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-10-27 13:20:49 -0400

    error_estimation: avoid star imports

--- pyroteus/error_estimation.py
+++ pyroteus/error_estimation.py
@@ -1,5 +1,6 @@
-from firedrake import *
+import firedrake
 from firedrake.petsc import PETSc
+import ufl
 
 
 __all__ = ["get_dwr_indicator"]
@@ -15,23 +16,26 @@ def form2indicator(F):
     :arg F: the form
     """
     mesh = F.ufl_domain()
-    P0 = FunctionSpace(mesh, "DG", 0)
-    p0test = TestFunction(P0)
-    indicator = Function(P0)
+    P0 = firedrake.FunctionSpace(mesh, "DG", 0)
+    p0test = firedrake.TestFunction(P0)
+    indicator = firedrake.Function(P0)
 
     # Contributions from surface integrals
     flux_terms = 0
     integrals = F.integrals_by_type('exterior_facet')
     if len(integrals) > 0:
         for integral in integrals:
-            flux_terms += p0test*integral.integrand()*ds(integral.subdomain_id())
+            ds = firedrake.ds(integral.subdomain_id())
+            flux_terms += p0test*integral.integrand()*ds
     integrals = F.integrals_by_type('interior_facet')
     if len(integrals) > 0:
         for integral in integrals:
-            flux_terms += p0test('+')*integral.integrand()*dS(integral.subdomain_id())
-            flux_terms += p0test('-')*integral.integrand()*dS(integral.subdomain_id())
+            dS = firedrake.dS(integral.subdomain_id())
+            flux_terms += p0test('+')*integral.integrand()*dS
+            flux_terms += p0test('-')*integral.integrand()*dS
     if flux_terms != 0:
-        mass_term = TrialFunction(P0)*p0test*dx
+        dx = firedrake.dx
+        mass_term = firedrake.TrialFunction(P0)*p0test*dx
         sp = {
             "mat_type": "matfree",
             "snes_type": "ksponly",
@@ -39,15 +43,16 @@ def form2indicator(F):
             "pc_type": "python",
             "pc_python_type": "firedrake.MassInvPC",
         }
-        solve(mass_term == flux_terms, indicator, solver_parameters=sp)
+        firedrake.solve(mass_term == flux_terms, indicator, solver_parameters=sp)
 
     # Contributions from volume integrals
     cell_terms = 0
     integrals = F.integrals_by_type('cell')
     if len(integrals) > 0:
         for integral in integrals:
-            cell_terms += p0test*integral.integrand()*dx(integral.subdomain_id())
-    indicator += assemble(cell_terms)
+            dx = firedrake.dx(integral.subdomain_id())
+            cell_terms += p0test*integral.integrand()*dx
+    indicator += firedrake.assemble(cell_terms)
 
     return indicator
 
@@ -86,4 +91,4 @@ def get_dwr_indicator(F, adjoint_error):
     fs = adjoint_error.function_space()
     if F.ufl_domain() != fs.mesh():
         raise ValueError("Meshes underlying the form and adjoint error do not match.")
-    return form2indicator(replace(F, {TestFunction(fs): adjoint_error}))
+    return form2indicator(ufl.replace(F, {firedrake.TestFunction(fs): adjoint_error}))
