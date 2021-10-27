4c35febf788794f495eba2677ffe1a128c6b7418
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-10-27 14:39:50 -0400

    recovery: avoid star imports

--- pyroteus/recovery.py
+++ pyroteus/recovery.py
@@ -4,6 +4,7 @@ Driver functions for derivative recovery.
 from __future__ import absolute_import
 from .interpolation import clement_interpolant
 from .utility import *
+from petsc4py import PETSc as petsc4py
 
 
 __all__ = ["recover_hessian", "recover_boundary_hessian"]
@@ -20,8 +21,10 @@ def recover_hessian(f, method='L2', **kwargs):
         g, H = double_l2_projection(f, **kwargs)
     elif method.capitalize() == 'Clement':
         mesh = kwargs.get('mesh') or f.function_space().mesh()
-        g = clement_interpolant(interpolate(grad(f), VectorFunctionSpace(mesh, "DG", 0)))
-        H = clement_interpolant(interpolate(grad(g), TensorFunctionSpace(mesh, "DG", 0)))
+        g = clement_interpolant(firedrake.interpolate(ufl.grad(f),
+                                firedrake.VectorFunctionSpace(mesh, "DG", 0)))
+        H = clement_interpolant(firedrake.interpolate(ufl.grad(g),
+                                firedrake.TensorFunctionSpace(mesh, "DG", 0)))
     elif method.upper() == 'ZZ':
         raise NotImplementedError("Zienkiewicz-Zhu recovery not yet implemented.")  # TODO
     else:
@@ -44,25 +47,28 @@ def double_l2_projection(f, mesh=None, target_spaces=None, mixed=False):
     """
     mesh = mesh or f.function_space().mesh()
     if target_spaces is None:
-        P1_vec = VectorFunctionSpace(mesh, "CG", 1)
-        P1_ten = TensorFunctionSpace(mesh, "CG", 1)
+        P1_vec = firedrake.VectorFunctionSpace(mesh, "CG", 1)
+        P1_ten = firedrake.TensorFunctionSpace(mesh, "CG", 1)
     else:
         P1_vec, P1_ten = target_spaces
     if not mixed:
-        g = project(grad(f), P1_vec)
-        H = project(grad(g), P1_ten)
+        g = firedrake.project(ufl.grad(f), P1_vec)
+        H = firedrake.project(ufl.grad(g), P1_ten)
         return g, H
     W = P1_vec*P1_ten
-    g, H = TrialFunctions(W)
-    phi, tau = TestFunctions(W)
-    l2_projection = Function(W)
-    n = FacetNormal(mesh)
+    g, H = firedrake.TrialFunctions(W)
+    phi, tau = firedrake.TestFunctions(W)
+    l2_projection = firedrake.Function(W)
+    n = ufl.FacetNormal(mesh)
 
     # The formulation is chosen such that f does not need to have any
     # finite element derivatives
-    a = inner(tau, H)*dx + inner(div(tau), g)*dx - dot(g, dot(tau, n))*ds
-    a += inner(phi, g)*dx
-    L = f*dot(phi, n)*ds - f*div(phi)*dx
+    a = ufl.inner(tau, H)*ufl.dx \
+        + ufl.inner(ufl.div(tau), g)*ufl.dx \
+        - ufl.dot(g, ufl.dot(tau, n))*ufl.ds
+    a += ufl.inner(phi, g)*ufl.dx
+    L = f*ufl.dot(phi, n)*ufl.ds \
+        - f*ufl.div(phi)*ufl.dx
 
     # Apply stationary preconditioners in the Schur complement to get away
     # with applying GMRES to the whole mixed system
@@ -80,7 +86,7 @@ def double_l2_projection(f, mesh=None, target_spaces=None, mixed=False):
         "fieldsplit_1_pc_type": "gamg",
         "fieldsplit_1_mg_levels_ksp_max_it": 5,
     }
-    if COMM_WORLD.size == 1:
+    if firedrake.COMM_WORLD.size == 1:
         sp["fieldsplit_0_pc_type"] = "ilu"
         sp["fieldsplit_1_mg_levels_pc_type"] = "ilu"
     else:
@@ -91,12 +97,12 @@ def double_l2_projection(f, mesh=None, target_spaces=None, mixed=False):
         sp["fieldsplit_1_mg_levels_sub_ksp_type"] = "preonly"
         sp["fieldsplit_1_mg_levels_sub_pc_type"] = "ilu"
     try:
-        solve(a == L, l2_projection, solver_parameters=sp)
-    except ConvergenceError:
-        PETSc.Sys.Print("L2 projection failed to converge with"
-                        " iterative solver parameters, trying direct.")
+        firedrake.solve(a == L, l2_projection, solver_parameters=sp)
+    except firedrake.ConvergenceError:
+        petsc4py.Sys.Print("L2 projection failed to converge with"
+                           " iterative solver parameters, trying direct.")
         sp = {"pc_mat_factor_solver_type": "mumps"}
-        solve(a == L, l2_projection, solver_parameters=sp)
+        firedrake.solve(a == L, l2_projection, solver_parameters=sp)
     return l2_projection.split()
 
 
@@ -121,20 +127,22 @@ def recover_boundary_hessian(f, mesh, method='Clement', target_space=None, **kwa
     assert d in (2, 3)
 
     # Apply Gram-Schmidt to get tangent vectors
-    n = FacetNormal(mesh)
+    n = ufl.FacetNormal(mesh)
     s = construct_orthonormal_basis(n)
-    ns = as_vector([n, *s])
+    ns = ufl.as_vector([n, *s])
 
     # Setup
-    P1 = FunctionSpace(mesh, "CG", 1)
-    P1_ten = target_space or TensorFunctionSpace(mesh, "CG", 1)
+    P1 = firedrake.FunctionSpace(mesh, "CG", 1)
+    P1_ten = target_space or firedrake.TensorFunctionSpace(mesh, "CG", 1)
     assert P1_ten.ufl_element().family() == 'Lagrange'
     assert P1_ten.ufl_element().degree() == 1
     boundary_tag = kwargs.get('boundary_tag', 'on_boundary')
-    Hs, v = TrialFunction(P1), TestFunction(P1)
-    l2_proj = [[Function(P1) for i in range(d-1)] for j in range(d-1)]
-    h = interpolate(CellSize(mesh), FunctionSpace(mesh, "DG", 0))
-    h = Constant(1/h.vector().gather().max()**2)
+    Hs = firedrake.TrialFunction(P1)
+    v = firedrake.TestFunction(P1)
+    l2_proj = [[firedrake.Function(P1) for i in range(d-1)] for j in range(d-1)]
+    h = firedrake.interpolate(ufl.CellDiameter(mesh),
+                              firedrake.FunctionSpace(mesh, "DG", 0))
+    h = firedrake.Constant(1/h.vector().gather().max()**2)
     f.pop('interior')
     sp = {
         "ksp_type": "gmres",
@@ -146,70 +154,71 @@ def recover_boundary_hessian(f, mesh, method='Clement', target_space=None, **kwa
     if method.upper() == 'L2':
 
         # Arbitrary value on domain interior
-        a = v*Hs*dx
-        L = v*h*dx
+        a = v*Hs*ufl.dx
+        L = v*h*ufl.dx
 
         # Hessian on boundary
-        nullspace = VectorSpaceBasis(constant=True)
+        nullspace = firedrake.VectorSpaceBasis(constant=True)
         for j, s1 in enumerate(s):
             for i, s0 in enumerate(s):
                 bcs = []
                 for tag, fi in f.items():
-                    a_bc = v*Hs*ds(tag)
-                    L_bc = -dot(s0, grad(v))*dot(s1, grad(fi))*ds(tag)
-                    bcs.append(EquationBC(a_bc == L_bc, l2_proj[i][j], tag))
-                solve(a == L, l2_proj[i][j], bcs=bcs,
-                      nullspace=nullspace, solver_parameters=sp)
+                    a_bc = v*Hs*ufl.ds(tag)
+                    L_bc = -ufl.dot(s0, ufl.grad(v))*ufl.dot(s1, ufl.grad(fi))*ufl.ds(tag)
+                    bcs.append(firedrake.EquationBC(a_bc == L_bc, l2_proj[i][j], tag))
+                firedrake.solve(a == L, l2_proj[i][j], bcs=bcs,
+                                nullspace=nullspace, solver_parameters=sp)
 
     elif method.capitalize() == 'Clement':
-        P0_vec = VectorFunctionSpace(mesh, "DG", 0)
-        P0_ten = TensorFunctionSpace(mesh, "DG", 0)
-        P1_vec = VectorFunctionSpace(mesh, "CG", 1)
-        H = Function(P1_ten)
-        p0test = TestFunction(P0_vec)
-        p1test = TestFunction(P1)
+        P0_vec = firedrake.VectorFunctionSpace(mesh, "DG", 0)
+        P0_ten = firedrake.TensorFunctionSpace(mesh, "DG", 0)
+        P1_vec = firedrake.VectorFunctionSpace(mesh, "CG", 1)
+        H = firedrake.Function(P1_ten)
+        p0test = firedrake.TestFunction(P0_vec)
+        p1test = firedrake.TestFunction(P1)
         fa = get_facet_areas(mesh)
         for tag, fi in f.items():
-            source = assemble(inner(p0test, grad(fi))/fa*ds)
+            source = firedrake.assemble(ufl.inner(p0test, ufl.grad(fi))/fa*ufl.ds)
 
             # Recover gradient
             c = clement_interpolant(source, boundary_tag=tag, target_space=P1_vec)
 
             # Recover Hessian
-            H += clement_interpolant(interpolate(grad(c), P0_ten),
+            H += clement_interpolant(firedrake.interpolate(ufl.grad(c), P0_ten),
                                      boundary_tag=tag, target_space=P1_ten)
 
         # Compute tangential components
         for j, s1 in enumerate(s):
             for i, s0 in enumerate(s):
-                l2_proj[i][j] = assemble(p1test*dot(dot(s0, H), s1)/fa*ds)
+                l2_proj[i][j] = firedrake.assemble(p1test*ufl.dot(ufl.dot(s0, H), s1)/fa*ufl.ds)
     else:
         raise ValueError(f"Recovery method '{method}' not supported"
                          " for Hessians on the boundary.")
 
     # Construct tensor field
-    Hbar = Function(P1_ten)
+    Hbar = firedrake.Function(P1_ten)
     if d == 2:
-        Hsub = interpolate(abs(l2_proj[0][0]), P1)
-        H = as_matrix([[h, 0],
-                       [0, Hsub]])
+        Hsub = firedrake.interpolate(abs(l2_proj[0][0]), P1)
+        H = ufl.as_matrix([[h, 0],
+                           [0, Hsub]])
     else:
-        Hsub = Function(TensorFunctionSpace(mesh, "CG", 1, shape=(2, 2)))
-        Hsub.interpolate(as_matrix([[l2_proj[0][0], l2_proj[0][1]],
-                                    [l2_proj[1][0], l2_proj[1][1]]]))
+        Hsub = firedrake.Function(firedrake.TensorFunctionSpace(mesh, "CG", 1, shape=(2, 2)))
+        Hsub.interpolate(ufl.as_matrix([[l2_proj[0][0], l2_proj[0][1]],
+                                        [l2_proj[1][0], l2_proj[1][1]]]))
         Hsub = hessian_metric(Hsub)
-        H = as_matrix([[h, 0, 0],
-                       [0, Hsub[0, 0], Hsub[0, 1]],
-                       [0, Hsub[1, 0], Hsub[1, 1]]])
+        H = ufl.as_matrix([[h, 0, 0],
+                           [0, Hsub[0, 0], Hsub[0, 1]],
+                           [0, Hsub[1, 0], Hsub[1, 1]]])
 
     # Arbitrary value on domain interior
-    sigma, tau = TrialFunction(P1_ten), TestFunction(P1_ten)
-    a = inner(tau, sigma)*dx
-    L = inner(tau, h*Identity(d))*dx
+    sigma = firedrake.TrialFunction(P1_ten)
+    tau = firedrake.TestFunction(P1_ten)
+    a = ufl.inner(tau, sigma)*ufl.dx
+    L = ufl.inner(tau, h*ufl.Identity(d))*ufl.dx
 
     # Boundary values imposed as in [Loseille et al. 2011]
-    a_bc = inner(tau, sigma)*ds
-    L_bc = inner(tau, dot(transpose(ns), dot(H, ns)))*ds
-    bcs = EquationBC(a_bc == L_bc, Hbar, boundary_tag)
-    solve(a == L, Hbar, bcs=bcs, solver_parameters=sp)
+    a_bc = ufl.inner(tau, sigma)*ufl.ds
+    L_bc = ufl.inner(tau, ufl.dot(ufl.transpose(ns), ufl.dot(H, ns)))*ufl.ds
+    bcs = firedrake.EquationBC(a_bc == L_bc, Hbar, boundary_tag)
+    firedrake.solve(a == L, Hbar, bcs=bcs, solver_parameters=sp)
     return Hbar
