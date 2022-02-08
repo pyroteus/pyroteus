05f8fcb12fe90b2267df051d2739e6d44a545b8b
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-02-08 13:19:13 +0000

    setup: apply black

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 
 setup(
-    name='pyroteus',
-    description='Pyroteus Goal-Oriented Mesh Adaptation Toolkit',
-    author='Joseph Wallwork',
-    author_email='j.wallwork16@imperial.ac.uk',
-    url='https://github.com/pyroteus/pyroteus',
-    version='0.1',
-    packages=['pyroteus', 'pyroteus_adjoint'],
+    name="pyroteus",
+    description="Pyroteus Goal-Oriented Mesh Adaptation Toolkit",
+    author="Joseph Wallwork",
+    author_email="j.wallwork16@imperial.ac.uk",
+    url="https://github.com/pyroteus/pyroteus",
+    version="0.1",
+    packages=["pyroteus", "pyroteus_adjoint"],
 )
