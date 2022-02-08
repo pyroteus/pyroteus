378285ccaf4faf453fcb303681482a0683ef7fb9
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-02-08 13:18:47 +0000

    install: apply black

--- install/configure_petsc.py
+++ install/configure_petsc.py
@@ -6,33 +6,32 @@ Modified from the automatically generated script found in `$PETSC_DIR/$PETSC_ARC
 """
 import sys
 import os
-sys.path.insert(0, os.path.abspath('config'))
+
+sys.path.insert(0, os.path.abspath("config"))
 import configure
 
 
 configure_options = [
-    'PETSC_ARCH=arch-pragmatic',
-    '--with-shared-libraries=1',
-    '--with-debugging=0',
-    '--with-fortran-bindings=0',
-    '--with-cxx-dialect=C++11',
-
-    '--download-zlib',
-    '--download-metis',
-    '--download-parmetis',
-    '--download-hdf5',
-    '--download-scalapack',
-    '--download-mumps',
-    '--download-triangle',
-    '--download-ctetgen',
-    '--download-chaco',
-    '--download-hypre',
-    '--download-eigen',
-    '--download-pragmatic',
-
-    '--with-mpiexec=/usr/bin/mpiexec.mpich',
-    '--CC=/usr/bin/mpicc.mpich',
-    '--CXX=/usr/bin/mpicxx.mpich',
-    '--FC=/usr/bin/mpif90.mpich',
+    "PETSC_ARCH=arch-pragmatic",
+    "--with-shared-libraries=1",
+    "--with-debugging=0",
+    "--with-fortran-bindings=0",
+    "--with-cxx-dialect=C++11",
+    "--download-zlib",
+    "--download-metis",
+    "--download-parmetis",
+    "--download-hdf5",
+    "--download-scalapack",
+    "--download-mumps",
+    "--download-triangle",
+    "--download-ctetgen",
+    "--download-chaco",
+    "--download-hypre",
+    "--download-eigen",
+    "--download-pragmatic",
+    "--with-mpiexec=/usr/bin/mpiexec.mpich",
+    "--CC=/usr/bin/mpicc.mpich",
+    "--CXX=/usr/bin/mpicxx.mpich",
+    "--FC=/usr/bin/mpif90.mpich",
 ]
 configure.petsc_configure(configure_options)
