2f5b6ee299e3e67212e360eedb8fd78f4c715472
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2022-06-13 11:22:43 +0100

    plot: plotting for steady-state

--- pyroteus/plot.py
+++ pyroteus/plot.py
@@ -28,18 +28,18 @@ def plot_snapshots(solutions, time_partition, field, label, **kwargs):
     P = time_partition
     rows = P.exports_per_subinterval[0] - 1
     cols = P.num_subintervals
-    fig, axes = plt.subplots(rows, cols, sharex="col", figsize=(6 * cols, 24 // cols))
+    steady = rows == cols == 1
+    figsize = kwargs.pop("figsize", (6 * cols, 24 // cols))
+    fig, axes = plt.subplots(rows, cols, sharex="col", figsize=figsize)
     for i, sols_step in enumerate(solutions[field][label]):
-        ax = axes[0] if cols == 1 else axes[0, i]
+        ax = axes if steady else axes[0] if cols == 1 else axes[0, i]
         ax.set_title(f"Mesh[{i}]")
         for j, sol in enumerate(sols_step):
-            ax = axes[j] if cols == 1 else axes[j, i]
+            ax = axes if steady else axes[j] if cols == 1 else axes[j, i]
             tricontourf(sol, axes=ax, **kwargs)
-            ax.annotate(
-                f"t={i*P.end_time/cols + j*P.timesteps_per_export[i]*P.timesteps[i]:.2f}",
-                (0.05, 0.05),
-                color="white",
-            )
+            if not steady:
+                time = i * P.end_time/cols + j * P.timesteps_per_export[i] * P.timesteps[i]
+                ax.annotate(f"t={time:.2f}", (0.05, 0.05), color="white")
     plt.tight_layout()
     return fig, axes
 
@@ -60,17 +60,17 @@ def plot_indicator_snapshots(indicators, time_partition, **kwargs):
     P = time_partition
     rows = P.exports_per_subinterval[0] - 1
     cols = P.num_subintervals
-    fig, axes = plt.subplots(rows, cols, sharex="col", figsize=(6 * cols, 24 // cols))
+    steady = rows == cols == 1
+    figsize = kwargs.pop("figsize", (6 * cols, 24 // cols))
+    fig, axes = plt.subplots(rows, cols, sharex="col", figsize=figsize)
     for i, indi_step in enumerate(indicators):
-        ax = axes[0] if cols == 1 else axes[0, i]
+        ax = axes if steady else axes[0] if cols == 1 else axes[0, i]
         ax.set_title(f"Mesh[{i}]")
         for j, indi in enumerate(indi_step):
-            ax = axes[j] if cols == 1 else axes[j, i]
+            ax = axes if steady else axes[j] if cols == 1 else axes[j, i]
             tricontourf(indi, axes=ax, **kwargs)
-            ax.annotate(
-                f"t={i*P.end_time/cols + j*P.timesteps_per_export[i]*P.timesteps[i]:.2f}",
-                (0.05, 0.05),
-                color="white",
-            )
+            if not steady:
+                time = i * P.end_time/cols + j * P.timesteps_per_export[i] * P.timesteps[i]
+                ax.annotate(f"t={time:.2f}", (0.05, 0.05), color="white")
     plt.tight_layout()
     return fig, axes
