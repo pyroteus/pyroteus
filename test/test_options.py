0df28e7fb0448e025a6ff8430cc08f7400d2b488
Joe Wallwork <joe.wallwork@outlook.com>
2023-01-30 06:13:34 +0000

    test: unit tests for MetricParameters

--- test/test_options.py
+++ test/test_options.py
@@ -62,3 +62,90 @@ class TestAdaptParameters(unittest.TestCase):
             AdaptParameters({"element_rtol": "0.001"})
         msg = "Expected attribute 'element_rtol' to be of type 'float' or 'int', not 'str'."
         self.assertEqual(str(cm.exception), msg)
+
+
+class TestMetricParameters(unittest.TestCase):
+    """
+    Unit tests for the :class:`MetricParameters` class.
+    """
+
+    def setUp(self):
+        self.defaults = {
+            "h_min": 1.0e-30,
+            "h_max": 1.0e30,
+            "a_max": 1.0e30,
+            "p": 1.0,
+            "base_complexity": 200.0,
+            "target_complexity": 4000.0,
+            "num_ramp_iterations": 3,
+            "miniter": 3,
+            "maxiter": 35,
+            "element_rtol": 0.001,
+        }
+
+    def test_defaults(self):
+        ap = MetricParameters()
+        for key, value in self.defaults.items():
+            self.assertEqual(ap[key], value)
+
+    def test_str(self):
+        ap = MetricParameters()
+        self.assertEqual(str(ap), str(self.defaults))
+
+    def test_repr(self):
+        ap = MetricParameters()
+        expected = (
+            "MetricParameters(h_min=1e-30, h_max=1e+30, a_max=1e+30, p=1.0,"
+            " base_complexity=200.0, target_complexity=4000.0, num_ramp_iterations=3,"
+            " miniter=3, maxiter=35, element_rtol=0.001)"
+        )
+        self.assertEqual(repr(ap), expected)
+
+    def test_hmin_type_error(self):
+        with self.assertRaises(TypeError) as cm:
+            MetricParameters({"h_min": "1.0e-30"})
+        msg = "Expected attribute 'h_min' to be of type 'float' or 'int', not 'str'."
+        self.assertEqual(str(cm.exception), msg)
+
+    def test_hmax_type_error(self):
+        with self.assertRaises(TypeError) as cm:
+            MetricParameters({"h_max": "1.0e30"})
+        msg = "Expected attribute 'h_max' to be of type 'float' or 'int', not 'str'."
+        self.assertEqual(str(cm.exception), msg)
+
+    def test_amax_type_error(self):
+        with self.assertRaises(TypeError) as cm:
+            MetricParameters({"a_max": "1.0e30"})
+        msg = "Expected attribute 'a_max' to be of type 'float' or 'int', not 'str'."
+        self.assertEqual(str(cm.exception), msg)
+
+    def test_p_type_error(self):
+        with self.assertRaises(TypeError) as cm:
+            MetricParameters({"p": "1.0"})
+        msg = "Expected attribute 'p' to be of type 'float' or 'int', not 'str'."
+        self.assertEqual(str(cm.exception), msg)
+
+    def test_base_complexity_type_error(self):
+        with self.assertRaises(TypeError) as cm:
+            MetricParameters({"base_complexity": "200.0"})
+        msg = "Expected attribute 'base_complexity' to be of type 'float' or 'int', not 'str'."
+        self.assertEqual(str(cm.exception), msg)
+
+    def test_target_complexity_type_error(self):
+        with self.assertRaises(TypeError) as cm:
+            MetricParameters({"target_complexity": "4000.0"})
+        msg = "Expected attribute 'target_complexity' to be of type 'float' or 'int', not 'str'."
+        self.assertEqual(str(cm.exception), msg)
+
+    def test_ramp_iter_type_error(self):
+        with self.assertRaises(TypeError) as cm:
+            MetricParameters({"num_ramp_iterations": 3.0})
+        msg = (
+            "Expected attribute 'num_ramp_iterations' to be of type 'int', not"
+            " 'float'."
+        )
+        self.assertEqual(str(cm.exception), msg)
+
+
+if __name__ == "__main__":
+    unittest.main()
