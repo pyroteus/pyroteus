74e5b163d4027ff7e029e873292a4d961de21e53
Joe Wallwork <joe.wallwork@outlook.com>
2023-03-20 08:38:21 +0000

    options: export method

--- pyroteus/options.py
+++ pyroteus/options.py
@@ -1,4 +1,5 @@
 from .utility import AttrDict
+from firedrake.meshadapt import RiemannianMetric
 
 
 __all__ = [
@@ -67,23 +68,80 @@ class MetricParameters(AdaptParameters):
         """
         :arg parameters: dictionary of parameters to set
         """
-        self["h_min"] = 1.0e-30  # Minimum metric magnitude
-        self["h_max"] = 1.0e30  # Maximum metric magnitude
-        self["a_max"] = 1.0e30  # Maximum anisotropy
+        self["num_ramp_iterations"] = 3  # Number of iterations to ramp over
+        self["verbosity"] = -1  # -1 = silent, 10 = maximum
+
+        # --- Normalisation
+
         self["p"] = 1.0  # Metric normalisation order
         self["base_complexity"] = 200.0  # Base metric complexity
         self["target_complexity"] = 4000.0  # Target metric complexity
-        self["num_ramp_iterations"] = 3  # Number of iterations to ramp over
+
+        # --- Adaptation
+        self["no_insert"] = False  # Turn off node insertion
+        self["no_swap"] = False  # Turn off edge and face swapping
+        self["no_move"] = False  # Turn off node movement
+        self["no_surf"] = False  # Turn off surface meshing
+        self["num_parmmg_iterations"] = 3
+
+        # --- Post-processing
+        self["h_min"] = 1.0e-30  # Minimum metric magnitude
+        self["h_max"] = 1.0e30  # Maximum metric magnitude
+        self["a_max"] = 1.0e5  # Maximum anisotropy
+        self["restrict_anisotropy_first"] = False
+        self["hausdorff_number"] = 0.01  # Controls length scales
+        self["gradation_factor"] = 1.3  # Controls ratio between adjacent edge lengths
 
         super().__init__(parameters=parameters)
 
-        self._check_type("h_min", (float, int))
-        self._check_type("h_max", (float, int))
-        self._check_type("a_max", (float, int))
+        self._check_type("num_ramp_iterations", int)
+        self._check_type("verbosity", int)
         self._check_type("p", (float, int))
         self._check_type("base_complexity", (float, int))
         self._check_type("target_complexity", (float, int))
-        self._check_type("num_ramp_iterations", int)
+        self._check_type("h_min", (float, int))
+        self._check_type("h_max", (float, int))
+        self._check_type("a_max", (float, int))
+        self._check_type("hausdorff_number", (float, int))
+        self._check_type("gradation_factor", (float, int))
+        self._check_type("restrict_anisotropy_first", bool)
+        self._check_type("no_insert", bool)
+        self._check_type("no_swap", bool)
+        self._check_type("no_move", bool)
+        self._check_type("no_surf", bool)
+        self._check_type("num_parmmg_iterations", int)
+
+    def export(self, metric):
+        """
+        Set parameters appropriate to a given :class:`RiemannianMetric`.
+
+        :arg metric: the :class:`RiemannianMetric` to apply parameters to
+        """
+        if not isinstance(metric, RiemannianMetric):
+            raise TypeError(
+                f"{type(self)} can only be exported to RiemannianMetric,"
+                f" not '{type(metric)}'."
+            )
+        petsc_specific = (
+            "verbosity",
+            "p",
+            "target_complexity",
+            "no_insert",
+            "no_swap",
+            "no_move",
+            "no_surf",
+            "h_min",
+            "h_max",
+            "a_max",
+            "restrict_anisotropy_first",
+            "hausdorff_number",
+            "gradation_factor",
+        )
+        metric_parameters = {
+            "dm_plex_metric": {key: self[key] for key in petsc_specific}
+        }
+        metric_parameters["num_iterations"] = self["num_parmmg_iterations"]
+        metric.set_parameters(metric_parameters)
 
 
 class GoalOrientedParameters(AdaptParameters):
