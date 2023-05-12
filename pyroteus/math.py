1651f6894917e1a48368ef2575e9dd5868fefa1f
Joe Wallwork <joe.wallwork@outlook.com>
2023-05-12 14:40:21 +0100

    math: rework recursive polynomial, including fix

--- pyroteus/math.py
+++ pyroteus/math.py
@@ -5,11 +5,22 @@ import ufl
 __all__ = ["bessi0", "bessk0", "gram_schmidt", "construct_orthonormal_basis"]
 
 
-def _recurse(a, *val):
-    out = val[0]
-    if len(val) > 1:
-        out += a * _recurse(*val[1:])
-    return out
+def recursive_polynomial(x, coeffs):
+    r"""
+    Compute the polynomial
+
+    ..math::
+        a_0 + x (a_1 + x (a_2 + \dots + x a_n)),
+
+    with coefficients :math:`a_0,\dots,a_n` in a recursive manner.
+
+    :arg x: value at which to evaluate the polynomial
+    :arg coeffs: tuple of coefficients defining the polynomial
+    """
+    p = coeffs[0]
+    if len(coeffs) > 1:
+        p += x * recursive_polynomial(x, coeffs[1:])
+    return p
 
 
 def bessi0(x):
@@ -18,9 +29,17 @@ def bessi0(x):
 
     Code taken from :cite:`VVP+:92`.
     """
+    if isinstance(x, np.ndarray):
+        from numpy import abs, exp, sqrt
+
+        if np.isclose(x, 0).any():
+            raise ValueError("Cannot divide by zero.")
+    else:
+        from ufl import abs, exp, sqrt
+
     ax = abs(x)
-    expr1 = _recurse(
-        (x / 3.75) ** 2,
+    x1 = (x / 3.75) ** 2
+    coeffs1 = (
         1.0,
         3.5156229,
         3.0899424,
@@ -29,23 +48,26 @@ def bessi0(x):
         3.60768e-2,
         4.5813e-3,
     )
-    expr2 = (
-        ufl.exp(ax)
-        / ufl.sqrt(ax)
-        * _recurse(
-            3.75 / ax,
-            0.39894228,
-            1.328592e-2,
-            2.25319e-3,
-            -1.57565e-3,
-            9.16281e-3,
-            -2.057706e-2,
-            2.635537e-2,
-            -1.647633e-2,
-            3.92377e-3,
-        )
+    x2 = 3.75 / ax
+    coeffs2 = (
+        0.39894228,
+        1.328592e-2,
+        2.25319e-3,
+        -1.57565e-3,
+        9.16281e-3,
+        -2.057706e-2,
+        2.635537e-2,
+        -1.647633e-2,
+        3.92377e-3,
     )
-    return ufl.conditional(ax < 3.75, expr1, expr2)
+
+    expr1 = recursive_polynomial(x1, coeffs1)
+    expr2 = exp(ax) / sqrt(ax) * recursive_polynomial(x2, coeffs2)
+
+    if isinstance(x, np.ndarray):
+        return np.where(ax < 3.75, expr1, expr2)
+    else:
+        return ufl.conditional(ax < 3.75, expr1, expr2)
 
 
 def bessk0(x):
@@ -54,9 +76,18 @@ def bessk0(x):
 
     Code taken from :cite:`VVP+:92`.
     """
-    expr1 = -ufl.ln(x / 2.0) * bessi0(x)
-    expr1 += _recurse(
-        x * x / 4.0,
+    if isinstance(x, np.ndarray):
+        from numpy import log as ln
+        from numpy import exp, sqrt, where
+
+        if (x <= 0).any():
+            raise ValueError("Cannot take the logarithm of a non-positive number.")
+    else:
+        from ufl import exp, ln, sqrt
+        from ufl import conditional as where
+
+    x1 = x * x / 4.0
+    coeffs1 = (
         -0.57721566,
         0.42278420,
         0.23069756,
@@ -65,21 +96,19 @@ def bessk0(x):
         1.0750e-4,
         7.4e-6,
     )
-    expr2 = (
-        ufl.exp(-x)
-        / ufl.sqrt(x)
-        * _recurse(
-            2.0 / x,
-            1.25331414,
-            -7.832358e-2,
-            2.189568e-2,
-            -1.062446e-2,
-            5.87872e-3,
-            -2.51540e-3,
-            5.3208e-4,
-        )
+    x2 = 2.0 / x
+    coeffs2 = (
+        1.25331414,
+        -7.832358e-2,
+        2.189568e-2,
+        -1.062446e-2,
+        5.87872e-3,
+        -2.51540e-3,
+        5.3208e-4,
     )
-    return ufl.conditional(x > 2, expr2, expr1)
+    expr1 = -ln(x / 2.0) * bessi0(x) + recursive_polynomial(x1, coeffs1)
+    expr2 = exp(-x) / sqrt(x) * recursive_polynomial(x2, coeffs2)
+    return where(x <= 2, expr1, expr2)
 
 
 def gram_schmidt(*v, normalise=False):
