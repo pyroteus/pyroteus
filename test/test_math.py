afbedf035b28593ec7a4630647e853ce99e5b219
Joe Wallwork <joe.wallwork@outlook.com>
2023-05-13 15:03:52 +0100

    test: unit tests for orthogonalisation

--- test/test_math.py
+++ test/test_math.py
@@ -1,8 +1,15 @@
-from pyroteus.math import bessk0, bessi0, recursive_polynomial, gram_schmidt
+from firedrake import UnitTriangleMesh
+from pyroteus.math import (
+    bessk0,
+    bessi0,
+    construct_basis,
+    gram_schmidt,
+    recursive_polynomial,
+)
 import numpy as np
 from parameterized import parameterized
-import pytest
 import scipy as sp
+import ufl
 import unittest
 
 
@@ -50,27 +57,66 @@ class TestBessel(unittest.TestCase):
         self.assertTrue(np.allclose(bessk0(x), sp.special.k0(x)))
 
 
-@pytest.fixture(params=[2, 3])
-def dim(request):
-    return request.param
-
-
-@pytest.fixture(params=[True, False])
-def normalise(request):
-    return request.param
-
-
-def test_gram_schmidt_numpy(dim, normalise):
+class TestOrthogonalisation(unittest.TestCase):
     """
-    Apply Gram-Schmidt to a random set of
-    vectors and check that the result is
-    an orthogonal/orthonormal set.
+    Unit tests for orthogonalisation.
     """
-    np.random.seed(0)
-    v = [np.random.rand(dim) for i in range(dim)]
-    u = gram_schmidt(*v, normalise=True)
-    for i, ui in enumerate(u):
-        for j, uj in enumerate(u):
-            if not normalise and i == j:
-                continue
-            assert np.isclose(np.dot(ui, uj), 1.0 if i == j else 0.0)
+
+    def setUp(self):
+        np.random.seed(0)
+
+    def test_gram_schmidt_type_error_numpy(self):
+        with self.assertRaises(TypeError) as cm:
+            gram_schmidt(np.ones(2), [1, 2])
+        msg = (
+            "Inconsistent vector types:"
+            " '<class 'numpy.ndarray'>' vs. '<class 'list'>'."
+        )
+        self.assertEqual(str(cm.exception), msg)
+
+    def test_gram_schmidt_type_error_ufl(self):
+        x = ufl.SpatialCoordinate(UnitTriangleMesh())
+        with self.assertRaises(ValueError) as cm:
+            gram_schmidt(x, [1, 2])
+        msg = "Invalid type conversion: [1, 2] can not be converted to any UFL type."
+        self.assertEqual(str(cm.exception), msg)
+
+    @parameterized.expand([2, 3])
+    def test_gram_schmidt_orthonormal_numpy(self, dim):
+        v = np.random.rand(dim, dim)
+        u = np.array(gram_schmidt(*v, normalise=True))
+        self.assertTrue(np.allclose(u.transpose() @ u, np.eye(dim)))
+
+    @parameterized.expand([2, 3])
+    def test_gram_schmidt_nonorthonormal_numpy(self, dim):
+        v = np.random.rand(dim, dim)
+        u = gram_schmidt(*v, normalise=False)
+        for i, ui in enumerate(u):
+            for j, uj in enumerate(u):
+                if i != j:
+                    self.assertAlmostEqual(np.dot(ui, uj), 0)
+
+    def test_basis_shape_error_numpy(self):
+        with self.assertRaises(ValueError) as cm:
+            construct_basis(np.ones((1, 2)))
+        msg = "Expected a vector, got an array of shape (1, 2)."
+        self.assertEqual(str(cm.exception), msg)
+
+    def test_basis_dim_error_numpy(self):
+        with self.assertRaises(ValueError) as cm:
+            construct_basis(np.ones(0))
+        msg = "Dimension 0 not supported."
+        self.assertEqual(str(cm.exception), msg)
+
+    @parameterized.expand([2, 3])
+    def test_basis_orthonormal_numpy(self, dim):
+        u = np.array(construct_basis(np.random.rand(dim), normalise=True))
+        self.assertTrue(np.allclose(u.transpose() @ u, np.eye(dim)))
+
+    @parameterized.expand([2, 3])
+    def test_basis_orthogonal_numpy(self, dim):
+        u = construct_basis(np.random.rand(dim), normalise=False)
+        for i, ui in enumerate(u):
+            for j, uj in enumerate(u):
+                if i != j:
+                    self.assertAlmostEqual(np.dot(ui, uj), 0)
