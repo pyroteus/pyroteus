f8ea901a74d9af3fc7b78d061314431bea1afbb5
Joe Wallwork <joe.wallwork@outlook.com>
2023-06-29 08:47:01 +0100

    test: updates for test_metric_normalisation

--- test/test_metric_normalise.py
+++ test/test_metric_normalise.py
@@ -2,7 +2,6 @@
 Test metric normalisation functionality.
 """
 from firedrake import *
-from firedrake.meshadapt import RiemannianMetric
 from pyroteus import *
 from sensors import *
 from utility import uniform_mesh
@@ -25,33 +24,42 @@ class TestMetricNormalisation(unittest.TestCase):
         P1_ten = TensorFunctionSpace(mesh, "CG", 1)
         return RiemannianMetric(P1_ten)
 
+    def test_metric_parameters_type_error(self):
+        with self.assertRaises(TypeError) as cm:
+            space_time_normalise([self.simple_metric], self.time_partition, [1])
+        msg = (
+            "Expected metric_parameters to consist of dictionaries,"
+            " not objects of type '<class 'int'>'."
+        )
+        self.assertEqual(str(cm.exception), msg)
+
     def test_normalistion_order_unset_error(self):
         mp = {"dm_plex_metric_target_complexity": 1.0}
         with self.assertRaises(ValueError) as cm:
             space_time_normalise([self.simple_metric], self.time_partition, mp)
         msg = "Normalisation order 'dm_plex_metric_p' must be set."
-        assert str(cm.exception) == msg
+        self.assertEqual(str(cm.exception), msg)
 
     def test_normalisation_order_invalid_error(self):
         mp = {"dm_plex_metric_target_complexity": 1.0, "dm_plex_metric_p": 0.0}
         with self.assertRaises(ValueError) as cm:
             space_time_normalise([self.simple_metric], self.time_partition, mp)
         msg = "Normalisation order '0.0' should be one or greater or np.inf."
-        assert str(cm.exception) == msg
+        self.assertEqual(str(cm.exception), msg)
 
     def test_target_complexity_unset_error(self):
         mp = {"dm_plex_metric_p": 1.0}
         with self.assertRaises(ValueError) as cm:
             space_time_normalise([self.simple_metric], self.time_partition, mp)
         msg = "Target complexity 'dm_plex_metric_target_complexity' must be set."
-        assert str(cm.exception) == msg
+        self.assertEqual(str(cm.exception), msg)
 
     def test_target_complexity_zero_error(self):
         mp = {"dm_plex_metric_target_complexity": 0.0, "dm_plex_metric_p": 1.0}
         with self.assertRaises(ValueError) as cm:
             space_time_normalise([self.simple_metric], self.time_partition, mp)
         msg = "Target complexity '0.0' is not positive."
-        assert str(cm.exception) == msg
+        self.assertEqual(str(cm.exception), msg)
 
     def test_target_complexity_negative_error(self):
         mp = {"dm_plex_metric_target_complexity": -1.0, "dm_plex_metric_p": 1.0}
@@ -93,14 +101,14 @@ class TestMetricNormalisation(unittest.TestCase):
         P1_ten = TensorFunctionSpace(mesh, "CG", 1)
         M = RiemannianMetric(P1_ten)
         M.compute_hessian(sensor(*mesh.coordinates))
-        assert not np.isnan(M.dat.data).any()
+        self.assertFalse(np.isnan(M.dat.data).any())
         M_st = M.copy(deepcopy=True)
 
         # Apply both normalisation strategies
         M.set_parameters(metric_parameters)
         M.normalise()
         space_time_normalise([M_st], self.time_partition, metric_parameters)
-        assert not np.isnan(M_st.dat.data).any()
+        self.assertFalse(np.isnan(M_st.dat.data).any())
 
         # Check that the metrics coincide
-        assert np.isclose(errornorm(M, M_st), 0.0)
+        self.assertAlmostEqual(errornorm(M, M_st), 0)
